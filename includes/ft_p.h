a75a4c7e5fdf80b321b25f57ddb7d1e5e09ec05d
Francois-xavier BABIN <fbabin@e1r13p8.42.fr>
2019-08-11 23:16:11 +0200

    added include file

--- includes/ft_p.h
+++ includes/ft_p.h
@@ -6,11 +6,22 @@
 /*   By: fbabin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 22:02:19 by fbabin            #+#    #+#             */
-/*   Updated: 2019/08/11 22:02:42 by fbabin           ###   ########.fr       */
+/*   Updated: 2019/08/11 22:35:44 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_P_H
 # define FT_P_H
+
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "ft_printf.h"
+
+
+int			ft_atoi(const char *str);
 
 #endif
